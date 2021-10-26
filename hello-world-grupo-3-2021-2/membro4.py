cc526862ea37f9d80e980cc9071bb91c04564afb
kalleoOuriques <90047255+kalleoOuriques@users.noreply.github.com>
2021-10-26 11:27:42 -0300

    Add files via upload

---
+++ hello-world-grupo-3-2021-2/membro4.py
@@ -0,0 +1,3 @@
+def hello():
+	return "Olá, eu sou o membro4! Meu nome é Kalleo tenho 18 anos, meu hobbie é o surf.Tenho experiência em desenvolvimento web, espero exercitar a minha lógica de programação."
+ 
