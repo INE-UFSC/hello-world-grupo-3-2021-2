77ddd3e0b90860bea68c7970e8fafe3e210e90ea
“jose-goulart” <josevictor2000goulart@hotmail.com>
2021-10-26 11:23:07 -0300

    Apresentação membro 1

--- membro1.py
+++ membro1.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro1" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o José. Tenho 21 anos, moro em Tramandaí-RS, meu esporte favorito é futsal, e também pratico calistenia. \n Jogo de pc que joguei recentemente foi CS. Espero solidificar minha base de programação coma disciplina.\n" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
